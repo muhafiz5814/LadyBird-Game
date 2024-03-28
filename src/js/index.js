157c2f1a8c41c4fa906366ed81f05f2b6739e823
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-03-28 13:03:59 +0530

    Make the moving buttons work

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,26 @@ const leftBtn = document.querySelector('#left');
 const homeBtn = document.querySelector('#home');
 const turnClockwiseBtn = document.querySelector('#turnClockWise');
 const turnAntiClockwiseBtn = document.querySelector('#turnAntiClockWise');
+
+let xPos = 0
+let yPos = 0
+
+upBtn.addEventListener("click", () => {
+    if(xPos > 0) xPos -= 10
+    bug.style.top = `${xPos}px`
+})
+
+downBtn.addEventListener("click", () => {
+    if(xPos < 500) xPos += 10
+    bug.style.top = `${xPos}px`
+})
+
+rightBtn.addEventListener("click", () => {
+    if(yPos < 500) yPos += 10
+    bug.style.left = `${yPos}px`
+})
+
+leftBtn.addEventListener("click", () => {
+    if(yPos > 0) yPos -= 10
+    bug.style.left = `${yPos}px`
+})
